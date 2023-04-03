ce40fa5ad7aa55c8ef6fdbf3d66996ae43c01868
Jeansfelipe <44785873+Jeansfelipe@users.noreply.github.com>
2023-04-03 19:31:56 +0000

    Ajuste da arvore

--- T1/T1.js
+++ T1/T1.js
@@ -31,8 +31,10 @@ scene.add(plane);//adiciona o plano a cena ja como grid
 
 // create a tree
 
-var trunk = new THREE.CylinderGeometry(1, 1, 3);//medidas do tronco
-var leaves = new THREE.ConeGeometry(3,5);//medida das camadas da arvore
+var trunk = new THREE.CylinderGeometry(0.6, 0.6, 3);//medidas do tronco
+var leaves = new THREE.ConeGeometry(1.5,1.5);//medida das camadas da arvore
+var leaves2 = new THREE.ConeGeometry(2,2);//medida das camadas da arvore
+var leaves3 = new THREE.ConeGeometry(2.5,2.5);//medida das camadas da arvore
 
 // create the mesh
 var trunkMesh = new THREE.Mesh(trunk, new THREE.MeshPhongMaterial({
@@ -41,18 +43,18 @@ var trunkMesh = new THREE.Mesh(trunk, new THREE.MeshPhongMaterial({
 var leavesMesh = new THREE.Mesh(leaves, new THREE.MeshPhongMaterial({
     color: 0x00ff00
 }));//mesh da 1 camada
-var leaves2Mesh = new THREE.Mesh(leaves, new THREE.MeshPhongMaterial({
+var leaves2Mesh = new THREE.Mesh(leaves2, new THREE.MeshPhongMaterial({
     color: 0x00ff00
 }));//mesh da 2 camada
-var leaves3Mesh = new THREE.Mesh(leaves, new THREE.MeshPhongMaterial({
+var leaves3Mesh = new THREE.Mesh(leaves3, new THREE.MeshPhongMaterial({
     color: 0x00ff00
 }));//mesh da 3 camada
 
 // position the trunk. Set y to half of height of trunk
 trunkMesh.position.set(-2, 1.5, 0);//posição do tronco
-leavesMesh.position.set(0, 7.5, 0);//posição em relação ao tronco da 1 camada
-leaves2Mesh.position.set(0, 5.5, 0);//posição em relação ao tronco da 2 camada
-leaves3Mesh.position.set(0, 3.5, 0);//posição em relação ao tronco da 3 camada
+leavesMesh.position.set(0, 4, 0);//posição em relação ao tronco da 1 camada
+leaves2Mesh.position.set(0, 3, 0);//posição em relação ao tronco da 2 camada
+leaves3Mesh.position.set(0, 2, 0);//posição em relação ao tronco da 3 camada
 
 trunkMesh.castShadow = true;//sombra do tronco
 trunkMesh.receiveShadow = true;
