3662999f6401f0da7c019b3ef26196445db7a472
Ketleen Anne Faria <ketleen@ice.ufjf.br>
2023-04-08 02:11:55 +0000

    aviao pronto

--- T1/aviao.js
+++ T1/aviao.js
@@ -25,29 +25,88 @@ window.addEventListener('resize', function () { onWindowResize(camera, renderer)
 let axesHelper = new THREE.AxesHelper(12);
 scene.add(axesHelper);
 
+//INICIO
+
+//angulos
+var angle90 = THREE.MathUtils.degToRad(90); //cria o angulo 90º
+var speed = 0.08;
+var animationOn = true; // control if animation is on or of
+                                      
+
 // create the ground plane
 let plane = createGroundPlaneWired(20, 20);//plano criado com base em (libs/util/util.js tem esse codigo)
 scene.add(plane);//adiciona o plano a cena ja como grid
 
-// create a plane
-let base = new THREE.CylinderGeometry(1.5, 1, 10);
-base.rotateZ(-Math.PI * 0.5); //rotacinando em 90°
+// criando o avião
+var base = new THREE.CylinderGeometry(1.5, 1, 12); //cria base
+base.rotateZ(- angle90); //rotacinando o cilindro em 90°
 
-let window = new THREE.CapsuleGeometry( 1, 1, 4, 8 );
+var janela = new THREE.CapsuleGeometry( 1, 1, 20, 25 ); //cria janela
+janela.rotateZ(- angle90); //rotacinando a janela em 90°
 
+var detalhe = new THREE.RingGeometry( 0.5, 1.55, 32 ); //detalhe em amarelo da frente do aviao
+detalhe.rotateY( angle90);
 
-// create the mesh
-var trunkMesh = new THREE.Mesh(base, new THREE.MeshPhongMaterial({
-    color: 0x797D7F
-}));
+var circulo = new THREE.CircleGeometry( 1, 32 ); //circulo preto da frente do avião
+circulo.rotateY( angle90); //rotacinando  em 90°
 
-// position the trunk. Set y to half of height of trunk
-trunkMesh.position.set(0, 5, 0);//posição do tronco
+var cilindro = new THREE.CylinderGeometry(0.2, 0.2 ,2); //cilindro que gira da frente do avião
+cilindro.rotateZ(angle90);
 
+var helice = new THREE.PlaneGeometry(5, 0.5);//helice do avião
+helice.rotateY(- angle90);
 
+var asaLateralMaior = new THREE.BoxGeometry( 2, 14, 0.25 ); //asa maior que fica na lateral
+asaLateralMaior.rotateX(- angle90);
 
-scene.add(trunkMesh);
+var asaLateralMenor = new THREE.BoxGeometry( 1.5, 6, 0.25 ); // asa menor que fica mais atras do aviao
+asaLateralMenor.rotateX(- angle90);
 
+var asinha = new THREE.BoxGeometry( 1, 2.5, 0.25 ); //asa que fica por cima
+
+//Mesh's
+var baseMesh = new THREE.Mesh(base, new THREE.MeshPhongMaterial({color: 0x797D7F}));
+baseMesh.position.set(0, 5, 0);
+
+var janelaMesh = new THREE.Mesh(janela, new THREE.MeshPhongMaterial({color: 0x1E90FF}));
+janelaMesh.position.set(3, 1, 0);
+
+var detalheMesh = new THREE.Mesh(detalhe, new THREE.MeshPhongMaterial({color: 0xFFFF00, side: THREE.DoubleSide}));
+detalheMesh.position.set(5.99, 0, 0);
+
+var circuloMesh = new THREE.Mesh(circulo, new THREE.MeshPhongMaterial({color: 0x000000}));
+circuloMesh.position.set(6.01,0,0);
+
+var cilindroMesh = new THREE.Mesh(cilindro, new THREE.MeshPhongMaterial({color: 0x000000}));
+cilindroMesh.position.set(6.01,0,0);
+
+var heliceMesh = new THREE.Mesh(helice, new THREE.MeshPhongMaterial({color: 0xFFFF00, side: THREE.DoubleSide})); 
+heliceMesh.position.set(0.98, 0, 0);
+
+var asaLateralMaiorMesh = new THREE.Mesh(asaLateralMaior, new THREE.MeshPhongMaterial({color: 0x797D7F, side: THREE.DoubleSide}));
+asaLateralMaiorMesh.position.set(2, 0, 0);
+
+var asaLateralMenorMesh = new THREE.Mesh(asaLateralMenor, new THREE.MeshPhongMaterial({color: 0x797D7F, side: THREE.DoubleSide}));
+asaLateralMenorMesh.position.set(-5, 0, 0);
+
+var asinhaMesh = new THREE.Mesh(asinha, new THREE.MeshPhongMaterial({color: 0x797D7F, side: THREE.DoubleSide}));
+asinhaMesh.position.set(-5.5, 1, 0);
+
+scene.add(baseMesh);
+baseMesh.add(janelaMesh);
+baseMesh.add(detalheMesh);
+baseMesh.add(circuloMesh);
+baseMesh.add(cilindroMesh);
+baseMesh.add(asaLateralMaiorMesh);
+baseMesh.add(asaLateralMenorMesh);
+baseMesh.add(asinhaMesh);
+cilindroMesh.add(heliceMesh);
+
+function rotateCylinder() {
+  if (animationOn) {
+    cilindroMesh.rotation.x +=speed; //girando o cilindro pois a helice esta nele e irá girar junto
+  }
+}
 
 // Use this to show information onscreen
 let controls = new InfoBox();
@@ -61,6 +120,7 @@ controls.show();
 
 render();
 function render() {
+    rotateCylinder();
     requestAnimationFrame(render);
     renderer.render(scene, camera) // Render scene
 }
