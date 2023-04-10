67bd37d036781abda5e0a5c8a0ae2f5ccd23d654
Ketleen Anne Faria <ketleen@ice.ufjf.br>
2023-04-10 19:59:09 +0000

    voltando versao

--- T1/Plano.js
+++ T1/Plano.js
@@ -1,47 +1,44 @@
 import * as THREE from "three";
-import { scene } from "./T1.js";
+//import { scene } from "./T1.js";
 import { createGroundPlaneWired } from "../libs/util/util.js";
 
 var Plano = function () {
   // Cria-se dois planos que alternarão entre si
   // produzindo um efeito de chão infinito
-  let plano1 = createGroundPlaneWired(400, 80, 300, 50, 3, "dimgray", "gainsboro");
-  let plano2 = createGroundPlaneWired(400, 80, 300, 50, 3, "dimgray", "gainsboro");
-  plano2.position.set(400, 0, 0);
-  let limiteCriadorDePlano = -300;
-  let alternadorDePlano = true;
-  let proxPlano = -800;
-  let novaPosition = 0;
-
-
+  this.plano1 = createGroundPlaneWired(600, 600);
+  this.plano2 = createGroundPlaneWired(600, 600);
+  this.plano2.position.set(0, 0, -600);
+  this.limiteCriadorDePlano = -500;
+  this.alternadorDePlano = true;
+  this.proxPlano = -1200;
+  this.novaPosition = 0;
 
   // Inicia os dois planos
-  let init = () => {
-    scene.add(plano1);
-    scene.add(plano2);
+  this.init = () => {
+    scene.add(this.plano1);
+    scene.add(this.plano2);
   };
-
 
   // Roda o estado dos planos,
   // verificando se o avião atingiu o limite necessário
   // do cenário, para transladar o plano de tras
   // para a frente do próximo plano
-  let update = () => {
-    if (flyCamera.mesh.position.z < limiteCriadorDePlano) {
-      limiteCriadorDePlano -= 400;
-      if (alternadorDePlano) {
-        novaPosition = plano1.position.z + proxPlano;
-        plano1.position.set(novaPosition, 0, 0);
-        alternadorDePlano = false;
+  this.update = () => {
+    if (aviao.mesh.position.z < this.limiteCriadorDePlano) {
+      this.limiteCriadorDePlano -= 600;
+      if (this.alternadorDePlano) {
+        this.novaPosition = this.plano1.position.z + this.proxPlano;
+        this.plano1.position.set(0, 0, this.novaPosition);
+        this.alternadorDePlano = false;
       } else {
-        novaPosition = plano2.position.z + proxPlano;
-        plano2.position.set(novaPosition, 0, 0);
-        alternadorDePlano = true;
+        this.novaPosition = this.plano2.position.z + this.proxPlano;
+        this.plano2.position.set(0, 0, this.novaPosition);
+        this.alternadorDePlano = true;
       }
     }
   };
 
-  init();
+  this.init();
 };
 
 export default Plano;
