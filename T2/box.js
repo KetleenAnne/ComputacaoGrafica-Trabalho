831e0b258a98b78574c5703114c38cd643efe323
0ket <ketleen@ice.ufjf.br>
2023-05-20 15:33:08 -0300

    novo plano

--- T2/box.js
+++ T2/box.js
@@ -1,13 +1,15 @@
 import * as THREE from "three";
-export function createPlane(position){
+export function createPlane(){
     const material = new THREE.MeshLambertMaterial({ color: 'lightgreen'});
 
-    const base = new THREE.BoxGeometry(300, 0, 500);
-    const plano = new THREE.Mesh(base, material);
-    plano.position.add(position);
+    const plano = new THREE.Mesh(new THREE.BoxGeometry(300, 0, 500), material);
+    const lateralEsq = new THREE.Mesh(new THREE.BoxGeometry(0, 150, 500), material);
+    const lateralDir = new THREE.Mesh(new THREE.BoxGeometry(0, 150, 500), material);
 
-    const lateralEsq = new THREE.Mesh(new THREE.BoxGeometry(0,150,500), material);
-    baseMesh.position.set(-150, 15, 0);
+    plano.position.set(0, 0 , 0);
+    lateralEsq.position.set(-150, 75, 0);
+    lateralDir.position.set(150, 75, 0);
     plano.add(lateralEsq);
+    plano.add(lateralDir);
   return plano;
 } 
